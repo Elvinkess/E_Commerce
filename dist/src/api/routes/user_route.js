1cb13fce86ad8546719a0b397f6077627c516b4e
Elvinkess <agberiakessena@gmail.com>
2025-08-27 10:34:05 +0100

    Add field validator

--- dist/src/api/routes/user_route.js
+++ dist/src/api/routes/user_route.js
@@ -4,9 +4,11 @@ const user_controller_1 = require("../controllers/user_controller");
 const express_1 = require("express");
 const program_1 = require("../program");
 const user_1 = require("../../core/domain/entity/user");
+const validator_1 = require("../middleware/validator");
 const userRoute = (0, express_1.Router)();
-let userController = new user_controller_1.UserController(program_1.userLogic);
-userRoute.post("/", userController.createUser);
+const validator = new validator_1.Validator();
+const userController = new user_controller_1.UserController(program_1.userLogic);
+userRoute.post("/", validator.signValidation, validator.validate, userController.createUser);
 userRoute.post("/signin", userController.signInUser);
 userRoute.get("/protected", program_1.authmiddleware.authenticateJWT, program_1.authmiddleware.authorizeRole([user_1.UserRole.USER]), (req, res) => {
     res.json({ message: "You have access to this protected route!" });
--- src/api/routes/user_route.ts
+++ src/api/routes/user_route.ts
@@ -2,11 +2,13 @@ import { UserController } from "../controllers/user_controller"
 import { Router } from "express";
 import { authmiddleware, userLogic } from "../program";
 import { UserRole } from "../../core/domain/entity/user";
+import { Validator } from "../middleware/validator";
 
 
 const userRoute = Router();
-let userController = new UserController(userLogic)
-userRoute.post("/", userController.createUser)
+const validator = new Validator()
+const userController = new UserController(userLogic)
+userRoute.post("/",validator.signValidation,validator.validate, userController.createUser)
 userRoute.post("/signin", userController.signInUser)
 userRoute.get("/protected",authmiddleware.authenticateJWT, authmiddleware.authorizeRole([UserRole.USER]), (req, res) => {
     res.json({ message: "You have access to this protected route!" });
