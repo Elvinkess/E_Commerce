2d0ef2a69321fd56775693b14db5d47d904b04b1
Elvinkess <agberiakessena@gmail.com>
2025-09-12 15:07:54 +0100

    update .env for render connection

--- src/api/connection.ts
+++ src/api/connection.ts
@@ -16,9 +16,9 @@ dotenv.config();
 const AppDataSource = new DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL, //connection string for hosted DBs
-    host: "localhost",
+    host: process.env.HOSTNAME || "localhost",
     port: parseInt(process.env.PGPORT || "5432", 10),
-    username: "postgres",
+    username: process.env.USERNAME || "postgres",
     password:process.env.PASSWORD,
     database: process.env.DATABASE,
     entities: [UserConfig,CategoriesConfig, ProductConfig, InventoryConfig,CartConfig,CartItemConfig,OrderConfig,OrderItemConfig,OrderPaymentConfig,AddressConfig,DeliveryConfig],
