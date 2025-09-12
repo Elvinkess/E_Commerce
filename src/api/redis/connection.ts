5b9701b917b9bc4be8e11b6d32c7d047e85d0074
Elvinkess <agberiakessena@gmail.com>
2025-09-12 10:09:41 +0100

    adjust the conection url name

--- src/api/redis/connection.ts
+++ src/api/redis/connection.ts
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 console.log("REDIS_URL from env:", process.env.REDIS_URL);
 
 const redisClient = createClient({
-  url:process.env.REDIS_CONNECTION, /*"redis://localhost:6379"*/ // default local Redis
+  url:process.env.REDIS_URL, /*"redis://localhost:6379"*/ // default local Redis
   socket: {
     tls: true, // Required for Render Redis
     rejectUnauthorized: false // Sometimes needed if SSL cert causes issues
