064c3c3fac9d3b78e385d38d27c467c972db0317
Elvinkess <agberiakessena@gmail.com>
2025-09-13 15:11:15 +0100

    remove the port etc for pg connection

--- src/api/connection.ts
+++ src/api/connection.ts
@@ -18,10 +18,10 @@ console.log(`DATABASE_URL at runtime: '${process.env.DATABASE_URL}'`);
 const AppDataSource = new DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL, //connection string for hosted DBs
-     port: parseInt(process.env.PGPORT || "5432", 10),
-    username: process.env.USERNAME || "postgres",
-    password:process.env.PASSWORD_DB,
-    database: process.env.DATABASE,
+    //  port: parseInt(process.env.PGPORT || "5432", 10),
+    // username: process.env.USERNAME || "postgres",
+    // password:process.env.PASSWORD_DB,
+    // database: process.env.DATABASE,
     entities: [UserConfig,CategoriesConfig, ProductConfig, InventoryConfig,CartConfig,CartItemConfig,OrderConfig,OrderItemConfig,OrderPaymentConfig,AddressConfig,DeliveryConfig],
     synchronize: false,
     logging: false,
